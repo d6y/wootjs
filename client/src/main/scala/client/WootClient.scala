212b49063b5db131079fe20c380c735a92df7aef
Richard Dallaway <richard@dallaway.com>
2015-04-15 12:08:38 +0100

    Improve client code slightly. Should use \/ instead of try for nice things like leftMap

--- client/src/main/scala/client/WootClient.scala
+++ client/src/main/scala/client/WootClient.scala
@@ -15,6 +15,12 @@ class WootClient(
   // This is the client copy of the document
   var doc = WString.empty()
 
+  def openDocument(w: WString): Unit = {
+    println(s"Becoming document: $w")
+    doc = w
+    onLoad(doc.text)
+  }
+
   // The web socket works in terms of text
   type Json = String
 
@@ -40,40 +46,37 @@ class WootClient(
   // Ingesting a remote operation or document
   //
 
+  private[this] def applyOperation(op: Operation): Unit = {
+
+    if (op.from == doc.site) {
+      // We receive back operations we sent.
+      // Useful for confirming receipt, but no action required.
+      println(s"Ignoring echo of our own operation")
+    } else {
+      println(s"Applying $op")
+      val (ops, wstring) = doc.integrate(op)
+
+      // Become the updated document:
+      doc = wstring
+
+      // Side effects:
+      ops.foreach {
+        case InsertOp(ch, _) => onReceive(ch.alpha.toString, true,  doc.visibleIndexOf(ch.id))
+        case DeleteOp(ch, _) => onReceive(ch.alpha.toString, false, doc.visibleIndexOf(ch.id))
+      }
+    }
+  }
+
   @JSExport
   def ingest(json: Json): Unit = {
     println(s"Ingesting: $json")
 
     // We can be sent a whole WString or an individual Operation.
     // We'll simply try to decode each type:
-    val in = Try(read[Operation](json)) orElse Try(read[WString](json))
+    val result =
+      Try(read[Operation](json)).map(applyOperation) orElse
+        Try(read[WString](json)).map(openDocument)
 
-    in match {
-      case Success(w: WString) =>
-        println(s"Becoming new document: $w")
-        doc = w
-        onLoad(doc.text)
 
-      case Success(op: Operation) if op.from == doc.site =>
-        // We receive back operations we sent.
-        // Useful for confirming receipt, but no action required.
-        println(s"Ignoring echo of our own operation")
-
-      case Success(op: Operation) =>
-        println(s"Decoded Operation $op")
-        val (ops, wstring) = doc.integrate(op)
-
-        // Become the updated document:
-        doc = wstring
-
-        // Side effects:
-        ops.foreach {
-          case InsertOp(ch, _) => onReceive(ch.alpha.toString, true,  doc.visibleIndexOf(ch.id))
-          case DeleteOp(ch, _) => onReceive(ch.alpha.toString, false, doc.visibleIndexOf(ch.id))
-        }
-
-      case Failure(err) =>
-        println(s"Unrecognized $json -> $err")
-    }
   }
 }
